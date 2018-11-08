cd3f6fa5956d0017780812de0d694c72cbbfbc90
Sohel Akram <sohelakramsami@gmail.com>
2018-11-08 03:12:52 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs=require("fs")
+
+  var server = http.createServer(function(req, res){
+    res.statusCode = 200;
+    res.setHeader("Content-Type",'text/html');
+    fs.readFile('index.html',function(err,data){
+        if(err){
+            return console.log("File Read Error");
+            
+        }
+        res.end(data);
+    })
+    
+  });
+  server.listen(process.env.PORT || 3000, process.env.IP || 'localhost', function(){
+    console.log('Server running');
+  });
